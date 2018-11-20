36dba71be51835a4b77f45393cc3bb384d602add
songy23 <songya@google.com>
2018-11-20 13:50:56 -0800

    Rename common to data.

--- data/data.go
+++ data/data.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package common
+package data
 
 import (
 	commonpb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
