5e7f70699ff1e8c0d430a4e4c4b72ae466ea5949
Steven Karis <skaris@omnition.io>
2019-01-10 13:20:57 -0800

    Add config-based attributes to all spans passing through collector
    There is a usecase where all spans going through the collector should be
    tagged with some additional attribute information (e.g. env, pipeline).
    This solves for that usecase.
    
    Testing Done: unit tests

--- internal/collector/processor/multi_processor.go
+++ internal/collector/processor/multi_processor.go
@@ -19,27 +19,75 @@ import (
 	"strings"
 
 	agenttracepb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/trace/v1"
+	tracepb "github.com/census-instrumentation/opencensus-proto/gen-go/trace/v1"
 )
+
+type option func(*multiSpanProcessor)
+type preProcessFn func(*agenttracepb.ExportTraceServiceRequest, string)
 
 // MultiSpanProcessor enables processing on multiple processors.
 // For each incoming span batch, it calls ProcessSpans method on each span
 // processor one-by-one. It aggregates success/failures/errors from all of
 // them and reports the result upstream.
-type MultiSpanProcessor []SpanProcessor
+type multiSpanProcessor struct {
+	processors    []SpanProcessor
+	preProcessFns []preProcessFn
+}
 
-var _ SpanProcessor = (*MultiSpanProcessor)(nil)
+// NewMultiSpanProcessor creates a multiSpanProcessor from the variadic
+// list of passed SpanProcessors and options.
+func NewMultiSpanProcessor(procs []SpanProcessor, options ...option) SpanProcessor {
+	multiSpanProcessor := &MultiSpanProcessor{
+		processors: procs,
+	}
+	for _, opt := range options {
+		opt(multiSpan)
+	}
+	return multiSpanProcessor
+}
 
-// NewMultiSpanProcessor creates a MultiSpanProcessor from the variadic
-// list of passed SpanProcessors.
-func NewMultiSpanProcessor(procs ...SpanProcessor) MultiSpanProcessor {
-	return procs
+func WithPreProcessFn(preProcFn preProcessFn) option {
+	return func(msp *multiSpanProcessor) {
+		msp.preProcessFns = append(msp.preProcessFns, preProcessFn)
+	}
+}
+
+func WithAddAttributes(attributes map[string]*AttributeValue, overwrite bool) option {
+	return WithPreProcessFn(
+		func(batch *agenttracepb.ExportTraceServiceRequest, spanFormat string) {
+			if len(attributes) == 0 {
+				return
+			}
+			for _, span := range batch.Spans {
+				if span.Attributes == nil {
+					span.Attributes = &tracepb.Span_Attributes{}
+				}
+				// Create a new map if one does not exist. Could re-use passed in map, but
+				// feels too unsafe.
+				if span.Attributes.AttributeMap == nil {
+					span.Attributes.AttributeMap = make(map[string]*AttributeValue, len(attributes))
+				}
+				// Copy all attributes that need to be added into the span's attribute map.
+				// If a key already exists, we will only overwrite is the overwrite flag
+				// is set to true.
+				for key, value := range attributes {
+					if _, exists := span.Attributes.AttributeMap[key]; overwrite || !exists {
+						span.Attributes.AttributeMap[key] = value
+					}
+				}
+			}
+		},
+	)
 }
 
 // ProcessSpans implements the SpanProcessor interface
-func (msp MultiSpanProcessor) ProcessSpans(batch *agenttracepb.ExportTraceServiceRequest, spanFormat string) (uint64, error) {
+func (msp *multiSpanProcessor) ProcessSpans(batch *agenttracepb.ExportTraceServiceRequest, spanFormat string) (uint64, error) {
+	for _, preProcessFn := range msp.preProcessFns {
+		preProcessFn(batch, spanFormat)
+	}
 	var maxFailures uint64
 	var errors []error
-	for _, sp := range msp {
+	for _, sp := range msp.processors {
 		failures, err := sp.ProcessSpans(batch, spanFormat)
 		if err != nil {
 			errors = append(errors, err)
