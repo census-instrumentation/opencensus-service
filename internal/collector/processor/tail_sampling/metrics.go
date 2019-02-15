b4c48901331a72bda4971b8ff8186710a6c05114
Steven Karis <skaris@omnition.io>
2019-02-15 11:18:35 -0800

    PR comments

--- internal/collector/processor/tail_sampling/metrics.go
+++ internal/collector/processor/tail_sampling/metrics.go
@@ -35,7 +35,7 @@ var (
 	statPolicyEvaluationErrorCount = stats.Int64("sampling_policy_evaluation_error", "Count of sampling policy evaluation errors", stats.UnitDimensionless)
 
 	statCountTracesSampled    = stats.Int64("count_traces_sampled", "Count of traces that were sampled", stats.UnitDimensionless)
-	statCountTracesNotSampled = stats.Int64("count_traces_not_sampled", "Count of traces that were sampled", stats.UnitDimensionless)
+	statCountTracesNotSampled = stats.Int64("count_traces_not_sampled", "Count of traces that were not sampled", stats.UnitDimensionless)
 
 	statDroppedTooEarlyCount    = stats.Int64("sampling_trace_dropped_too_early", "Count of traces that needed to be dropped the configured wait time", stats.UnitDimensionless)
 	statNewTraceIDReceivedCount = stats.Int64("new_trace_id_received", "Counts the arrival of new traces", stats.UnitDimensionless)
--- internal/collector/processor/tail_sampling/processor.go
+++ internal/collector/processor/tail_sampling/processor.go
@@ -251,11 +251,9 @@ func (tsp *tailSamplingSpanProcessor) ProcessSpans(batch *agenttracepb.ExportTra
 						zap.Uint64("failCount", failCount),
 						zap.Error(err))
 				}
-				stats.Record(policyAndDests.ctx, statCountTracesSampled.M(int64(1)))
 				fallthrough // so OnLateArrivingSpans is also called for decision Sampled.
 			case sampling.NotSampled:
 				policyAndDests.Evaluator.OnLateArrivingSpans(actualDecision, spans)
-				stats.Record(policyAndDests.ctx, statCountTracesNotSampled.M(int64(1)))
 				stats.Record(tsp.ctx, statLateSpanArrivalAfterDecision.M(int64(time.Since(actualData.DecisionTime)/time.Second)))
 
 			default:
