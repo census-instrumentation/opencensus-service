65b0108b975342cfde749715ad871c352c5edcb6
Bogdan Drutu <bogdandrutu@gmail.com>
2019-03-03 11:07:55 -0800

    Add tests for [trace|span]_id conversion from ints, and change code to use this.

--- translator/trace/big_endian_converter_test.go
+++ translator/trace/big_endian_converter_test.go
@@ -13,3 +13,121 @@
 // limitations under the License.
 
 package tracetranslator
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func TestUInt64TraceIdConversion(t *testing.T) {
+	if nil != UInt64ToByteTraceID(0, 0) {
+		t.Errorf("Failed to convert 0 TraceID:\n\tGot: %v\nWant: nil", UInt64ToByteTraceID(0, 0))
+	}
+	assertEqual(t, UInt64ToByteTraceID(256*256+256+1, 256+1),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01},
+		"Failed simple conversion:")
+	assertEqual(t, UInt64ToByteTraceID(0, 5),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05},
+		"Failed to convert 0 high:")
+	assertEqual(t, UInt64ToByteTraceID(5, 0),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		"Failed to convert 0 low:")
+	assertEqual(t, UInt64ToByteTraceID(math.MaxUint64, 5),
+		[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05},
+		"Failed to convert MaxUint64:")
+}
+
+func TestInt64TraceIdConversion(t *testing.T) {
+	if nil != Int64ToByteTraceID(0, 0) {
+		t.Errorf("Failed to convert 0 TraceID:\n\tGot: %v\nWant: nil", Int64ToByteTraceID(0, 0))
+	}
+	assertEqual(t, Int64ToByteTraceID(0, -1),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+		"Failed to convert negative low:")
+	assertEqual(t, Int64ToByteTraceID(-2, 5),
+		[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05},
+		"Failed to convert negative high:")
+	assertEqual(t, Int64ToByteTraceID(5, math.MinInt64),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		"Failed to convert MinInt64:")
+}
+
+func TestUInt64SpanIdConversion(t *testing.T) {
+	if nil != UInt64ToByteSpanID(0) {
+		t.Errorf("Failed to convert 0 SpanID:\n\tGot: %v\nWant: nil", UInt64ToByteSpanID(0))
+	}
+	assertEqual(t, UInt64ToByteSpanID(256*256+256+1),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01},
+		"Failed simple conversion:")
+	assertEqual(t, UInt64ToByteSpanID(math.MaxUint64),
+		[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+		"Failed to convert MaxUint64:")
+}
+
+func TestInt64SpanIdConversion(t *testing.T) {
+	if nil != Int64ToByteSpanID(0) {
+		t.Errorf("Failed to convert 0 SpanID:\n\tGot: %v\nWant: nil", Int64ToByteSpanID(0))
+	}
+	assertEqual(t, Int64ToByteSpanID(5),
+		[]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05},
+		"Failed to convert positive id:")
+	assertEqual(t, Int64ToByteSpanID(-1),
+		[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+		"Failed to convert negative id:")
+	assertEqual(t, Int64ToByteSpanID(math.MinInt64),
+		[]byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		"Failed to convert MinInt64:")
+}
+
+func TestTraceIDInt64RoundTrip(t *testing.T) {
+	wh := int64(0x70605040302010FF)
+	wl := int64(0x0001020304050607)
+	gh, gl, err := BytesToInt64TraceID(Int64ToByteTraceID(wh, wl))
+	if err != nil {
+		t.Errorf("Error converting from bytes TraceID: %v", err)
+	}
+	if gh != wh || gl != wl {
+		t.Errorf("Round trip of TraceID failed:\n\tGot: (0x%0x, 0x%0x)\n\tWant: (0x%0x, 0x%0x)", gl, gh, wl, wh)
+	}
+}
+
+func TestTraceIDUInt64RoundTrip(t *testing.T) {
+	wh := uint64(0x70605040302010FF)
+	wl := uint64(0x0001020304050607)
+	gh, gl, err := BytesToUInt64TraceID(UInt64ToByteTraceID(wh, wl))
+	if err != nil {
+		t.Errorf("Error converting from bytes TraceID: %v", err)
+	}
+	if gl != wl || gh != wh {
+		t.Errorf("Round trip of TraceID failed:\n\tGot: (0x%0x, 0x%0x)\n\tWant: (0x%0x, 0x%0x)", gl, gh, wl, wh)
+	}
+}
+
+func TestSpanIdInt64RoundTrip(t *testing.T) {
+	w := int64(0x0001020304050607)
+	g, err := BytesToInt64SpanID(Int64ToByteSpanID(w))
+	if err != nil {
+		t.Errorf("Error converting from OC span id: %v", err)
+	}
+	if g != w {
+		t.Errorf("Round trip of SpanId failed:\n\tGot: 0x%0x\n\tWant: 0x%0x", g, w)
+	}
+}
+
+func TestSpanIdUInt64RoundTrip(t *testing.T) {
+	w := uint64(0x0001020304050607)
+	g, err := BytesToUInt64SpanID(UInt64ToByteSpanID(w))
+	if err != nil {
+		t.Errorf("Error converting from OC span id: %v", err)
+	}
+	if g != w {
+		t.Errorf("Round trip of SpanId failed:\n\tGot: 0x%0x\n\tWant: 0x%0x", g, w)
+	}
+}
+
+func assertEqual(t *testing.T, got []byte, want []byte, em string) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("%s\n\tGot: %v\n\tWant: %v\n", em, got, want)
+	}
+}
