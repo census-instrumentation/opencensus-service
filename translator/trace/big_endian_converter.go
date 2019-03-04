f62ee26830f527d8a4b74bb74278192d3f0729ec
Bogdan Drutu <bogdandrutu@gmail.com>
2019-03-03 19:19:13 -0800

    Fix comments for public functions in big_endian_convertor.

--- translator/trace/big_endian_converter.go
+++ translator/trace/big_endian_converter.go
@@ -30,8 +30,8 @@ var (
 	ErrWrongLenSpanID = errors.New("SpanID does not have 8 bytes")
 )
 
-// UInt64ToByteTraceID takes a two uint64 representaition of a trace id
-// and converts it to a []byte representation.
+// UInt64ToByteTraceID takes a two uint64 representaition of a TraceID and
+// converts it to a []byte representation.
 func UInt64ToByteTraceID(high, low uint64) []byte {
 	if high == 0 && low == 0 {
 		return nil
@@ -42,13 +42,14 @@ func UInt64ToByteTraceID(high, low uint64) []byte {
 	return traceID
 }
 
-// Int64ToByteTraceID takes a two int64 representaition of a trace id
-// and converts it to a []byte representation.
+// Int64ToByteTraceID takes a two int64 representaition of a TraceID and
+// converts it to a []byte representation.
 func Int64ToByteTraceID(high, low int64) []byte {
 	return UInt64ToByteTraceID(uint64(high), uint64(low))
 }
 
-// BytesToUInt64TraceID ..
+// BytesToUInt64TraceID takes a []byte representation of a TraceID and
+// converts it to a two uint64 representation.
 func BytesToUInt64TraceID(traceID []byte) (uint64, uint64, error) {
 	if traceID == nil {
 		return 0, 0, ErrNilTraceID
@@ -59,13 +60,14 @@ func BytesToUInt64TraceID(traceID []byte) (uint64, uint64, error) {
 	return binary.BigEndian.Uint64(traceID[:8]), binary.BigEndian.Uint64(traceID[8:]), nil
 }
 
-// BytesToInt64TraceID ..
+// BytesToInt64TraceID takes a []byte representation of a TraceID and
+// converts it to a two int64 representation.
 func BytesToInt64TraceID(traceID []byte) (int64, int64, error) {
 	traceIDHigh, traceIDLow, err := BytesToUInt64TraceID(traceID)
 	return int64(traceIDHigh), int64(traceIDLow), err
 }
 
-// UInt64ToByteSpanID takes a uint64 representation of a span id and
+// UInt64ToByteSpanID takes a uint64 representation of a SpanID and
 // converts it to a []byte representation.
 func UInt64ToByteSpanID(id uint64) []byte {
 	if id == 0 {
@@ -76,13 +78,14 @@ func UInt64ToByteSpanID(id uint64) []byte {
 	return spanID
 }
 
-// Int64ToByteSpanID takes a int64 representation of a span id and
+// Int64ToByteSpanID takes a int64 representation of a SpanID and
 // converts it to a []byte representation.
 func Int64ToByteSpanID(id int64) []byte {
 	return UInt64ToByteSpanID(uint64(id))
 }
 
-// BytesToUInt64SpanID returns
+// BytesToUInt64SpanID takes a []byte representation of a SpanID and
+// converts it to a uint64 representation.
 func BytesToUInt64SpanID(b []byte) (uint64, error) {
 	if b == nil {
 		return 0, ErrNilSpanID
@@ -93,7 +96,8 @@ func BytesToUInt64SpanID(b []byte) (uint64, error) {
 	return binary.BigEndian.Uint64(b), nil
 }
 
-// BytesToInt64SpanID retrurns.
+// BytesToInt64SpanID takes a []byte representation of a SpanID and
+// converts it to a int64 representation.
 func BytesToInt64SpanID(b []byte) (int64, error) {
 	id, err := BytesToUInt64SpanID(b)
 	return int64(id), err
