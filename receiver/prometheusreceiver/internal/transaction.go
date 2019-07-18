d4f12f7109972216fc37f6c9f42edc4136a972fb
Dino Oliva <dinooliva@users.noreply.github.com>
2019-07-18 16:13:43 -0700

    Fix issues with initial start time and restarts in the Prometheus receiver. (#598)
    * Updates dependencies to use dinooliva/promreceiver
    
    * Adds metrics_asjuster for dealing with inital time series values and restarts.
    
    * Updates code to conform to contribution specs.
    
    * Update MetricsAdjuster to handle resets appropriately
    
    * Apply gofmt to non-conforming files
    
    * Make metrics adjustment configurable
    
    * Apply gofmt to metrics_receiver
    
    * Fix all issues related to fmt/vet/lint
    
    * Extend unit tests to include multiple simultaneous metrics and timeseries
    
    * Add unit test for implicit/explicit empty label values; rename vars for readability
    
    * Clarify naming (latest -> previous) for timeseriesinfo

--- receiver/prometheusreceiver/internal/transaction.go
+++ receiver/prometheusreceiver/internal/transaction.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	commonpb "github.com/census-instrumentation/opencensus-proto/gen-go/agent/common/v1"
 	"github.com/census-instrumentation/opencensus-service/consumer"
+	"github.com/census-instrumentation/opencensus-service/data"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/storage"
@@ -47,19 +48,24 @@ type transaction struct {
 	ctx           context.Context
 	isNew         bool
 	sink          consumer.MetricsConsumer
+	job           string
+	instance      string
+	jobsMap       *JobsMap
 	ms            MetadataService
+	node          *commonpb.Node
 	metricBuilder *metricBuilder
 	logger        *zap.SugaredLogger
 }
 
-func newTransaction(ctx context.Context, ms MetadataService, sink consumer.MetricsConsumer, logger *zap.SugaredLogger) *transaction {
+func newTransaction(ctx context.Context, jobsMap *JobsMap, ms MetadataService, sink consumer.MetricsConsumer, logger *zap.SugaredLogger) *transaction {
 	return &transaction{
-		id:     atomic.AddInt64(&idSeq, 1),
-		ctx:    ctx,
-		isNew:  true,
-		sink:   sink,
-		ms:     ms,
-		logger: logger,
+		id:      atomic.AddInt64(&idSeq, 1),
+		ctx:     ctx,
+		isNew:   true,
+		sink:    sink,
+		jobsMap: jobsMap,
+		ms:      ms,
+		logger:  logger,
 	}
 }
 
@@ -98,8 +104,12 @@ func (tr *transaction) initTransaction(ls labels.Labels) error {
 	if err != nil {
 		return err
 	}
-	node := createNode(job, instance, mc.SharedLabels().Get(model.SchemeLabel))
-	tr.metricBuilder = newMetricBuilder(node, mc, tr.logger)
+	if tr.jobsMap != nil {
+		tr.job = job
+		tr.instance = instance
+	}
+	tr.node = createNode(job, instance, mc.SharedLabels().Get(model.SchemeLabel))
+	tr.metricBuilder = newMetricBuilder(mc, tr.logger)
 	tr.isNew = false
 	return nil
 }
@@ -112,15 +122,21 @@ func (tr *transaction) Commit() error {
 		return nil
 	}
 
-	md, err := tr.metricBuilder.Build()
+	metrics, err := tr.metricBuilder.Build()
 	if err != nil {
 		return err
 	}
 
-	if md != dummyMetric {
-		return tr.sink.ConsumeMetricsData(context.Background(), *md)
+	if metrics != nil {
+		if tr.jobsMap != nil {
+			metrics = NewMetricsAdjuster(tr.jobsMap.get(tr.job, tr.instance), tr.logger).AdjustMetrics(metrics)
+		}
+		md := data.MetricsData{
+			Node:    tr.node,
+			Metrics: metrics,
+		}
+		return tr.sink.ConsumeMetricsData(context.Background(), md)
 	}
-
 	return nil
 }
 
